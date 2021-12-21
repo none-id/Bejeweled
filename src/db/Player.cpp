5c4c611992ae623904b111560c06f221604064dc
Zhang Caiyi <mail@zhangcaiyi.com>
2021-12-21 13:07:11 +0800

    Add id property to the Player class

--- src/db/Player.cpp
+++ src/db/Player.cpp
@@ -4,8 +4,16 @@
 
 namespace bjw {
 
-Player::Player(std::string username, std::string password)
-    : username(std::move(username)), password(std::move(password)), score(0) {}
+Player::Player(unsigned long long int id, std::string username,
+               std::string password, unsigned long long int score)
+    : id(id),
+      username(std::move(username)),
+      password(std::move(password)),
+      score(score) {}
+
+bool Player::VerifyPassword(std::string password) const {
+  return Player::password == password;
+}
 
 void Player::SetScore(unsigned long long int score) { Player::score = score; }
 
--- src/db/Player.h
+++ src/db/Player.h
@@ -7,14 +7,17 @@ namespace bjw {
 
 class Player {
  private:
+  unsigned long long int id;
   std::string username;
   std::string password;
   unsigned long long int score;
 
  public:
-  Player(std::string username, std::string password);
-  void SetScore(unsigned long long int score);
+  Player(unsigned long long int id, std::string username, std::string password,
+         unsigned long long int score);
+  bool VerifyPassword(std::string password) const;
   unsigned long long int GetScore() const;
+  void SetScore(unsigned long long int score);
 };
 
 }  // namespace bjw
