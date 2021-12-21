07724db5f57ec570645b0d26876bb54eabff9415
Noneid <qinyuelai128@yeah.net>
2021-12-21 22:44:07 +0800

    移子操作优化

--- src/GameJudge.cpp
+++ src/GameJudge.cpp
@@ -3,10 +3,39 @@
 namespace bjw {
 
 // 处理玩家移子操作
+// 接收数据格式：(x1, y1) (x2, y2) 对应两子交换前坐标
 void GameJudge::PdDeleteItem(int x1, int y1, int x2, int y2) {
-  int flag = 0;
-  FindLengthMore3(x1, y1, flag);
-  FindLengthMore3(x2, y2, flag);
+  int bingoNums = 0;
+  bingoGroup bgGFirst, bgGSecond;
+  std::vector<doGroup> doPdList;
+
+  swapDataInMap(x1, y1, x2, y2);
+
+  doPdList.emplace_back(x1, y1);
+  doPdList.emplace_back(x2, y2);
+
+  // TODO 实现每次以时间点为界限 发送对应修改后子集合
+  while (!doPdList.empty() || bingoNums == 0) {
+    switch (bingoNums) {
+      case 0:
+        // TODO 反馈非成功消去状态
+        break;
+      case 1:
+        if (bgGFirst.noInitFlag) {
+        } else {
+        }
+        // TODO 反馈单一成功消去状态
+        break;
+      case 2:
+
+        break;
+      default:
+        throw -1;
+    }
+
+    bingoNums += FindLengthMore3(x1, y1, bgGFirst);
+    bingoNums += FindLengthMore3(x2, y2, bgGSecond);
+  }
 }
 
 // 向UI传输数据
@@ -14,7 +43,7 @@ void GameJudge::SendData() {
   // TODO  接口协商  emit函数注册
 }
 
-void GameJudge::FindLengthMore3(int x, int y, int &flag) {
+bool GameJudge::FindLengthMore3(int x, int y, bingoGroup &bgG) {
   int sum = 0;
   int target_color = game_map[x][y].GetColorId();
   int start_x, end_x, start_y, end_y;
@@ -29,7 +58,10 @@ void GameJudge::FindLengthMore3(int x, int y, int &flag) {
 
   // 大于3 代表可以消去
   if (sum > 3) {
-    SendData();  // TODO 当前需消起止坐标传输
+    bgG.set(start_x, end_x, start_y, end_y);
+    return true;
+  } else {
+    return false;
   }
 }
 
@@ -54,11 +86,22 @@ int GameJudge::MapDfs(int now_x, int now_y, int target_color, int &t_min_x,
   if (now_y > t_max_y) t_max_y = now_y;
 
   // 上下左右四方向前进
-  sum += MapDfs(now_x + 1, now_y, target_color, t_min_x, t_min_y, t_max_x, t_max_y);
-  sum += MapDfs(now_x, now_y + 1, target_color, t_min_x, t_min_y, t_max_x, t_max_y);
-  sum += MapDfs(now_x - 1, now_y, target_color, t_min_x, t_min_y, t_max_x, t_max_y);
-  sum += MapDfs(now_x, now_y - 1, target_color, t_min_x, t_min_y, t_max_x, t_max_y);
+  sum += MapDfs(now_x + 1, now_y, target_color, t_min_x, t_min_y, t_max_x,
+                t_max_y);
+  sum += MapDfs(now_x, now_y + 1, target_color, t_min_x, t_min_y, t_max_x,
+                t_max_y);
+  sum += MapDfs(now_x - 1, now_y, target_color, t_min_x, t_min_y, t_max_x,
+                t_max_y);
+  sum += MapDfs(now_x, now_y - 1, target_color, t_min_x, t_min_y, t_max_x,
+                t_max_y);
 
   return sum;
+}
+
+// 交换地图内元素
+void GameJudge::swapDataInMap(int x1, int y1, int x2, int y2) {
+  Item temp_item(game_map[x1][y1]);
+  game_map[x1][y1] = game_map[x2][y2];
+  game_map[x2][y2] = temp_item;
 }
 }  // namespace bjw
