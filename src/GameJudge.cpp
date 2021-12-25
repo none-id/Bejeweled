94621ca708fa390a6a952dbab6fc03f9b4b8c490
秦悦来 <qinyuelai128@yeah.net>
2021-12-25 15:42:09 +0800

    format verbs

--- src/GameJudge.cpp
+++ src/GameJudge.cpp
@@ -6,59 +6,58 @@ namespace bjw {
 // 接收数据格式：(x1, y1) (x2, y2) 对应两子交换前坐标
 void GameJudge::PdDeleteItem(int x1, int y1, int x2, int y2) {
   int count = 0;
-  std::vector<doGroup> doPdList;
-  std::vector<std::vector<bingoGroup>> bingoList;
+  std::vector<do_group> next_do_list;
+  std::vector<bingo_group> bingo_list;
 
-  swapDataInMap(x1, y1, x2, y2);
+  SwapDataInMap(x1, y1, x2, y2);
 
-  doPdList.emplace_back(x1, y1);
-  doPdList.emplace_back(x2, y2);
+  next_do_list.emplace_back(x1, y1);
+  next_do_list.emplace_back(x2, y2);
 
-  while (!doPdList.empty()) {
-    int beforeSize = doPdList.size();
+  while (!next_do_list.empty()) {
+    int beforeSize = next_do_list.size();
 
     for (int i = 0; i < beforeSize; ++i) {
-      bingoGroup bgGTemp;
-      FindLengthMore3(doPdList[i].x, doPdList[i].y, bgGTemp);
-      if (bgGTemp.noInitFlag) {
-        bingoList[count].push_back(bgGTemp);
+      bingo_group bg_temp;
+      FindLengthMore3(next_do_list[i].x, next_do_list[i].y, bg_temp);
+      if (bg_temp.no_init_flag) {
+        bingo_list.push_back(bg_temp);
 
         int b_start_x, b_start_y, b_end_x, b_end_y;
-        b_start_x = bgGTemp.start_x;
-        b_start_y = bgGTemp.start_y;
-        b_end_x = bgGTemp.end_x;
-        b_end_y = bgGTemp.end_y;
+        b_start_x = bg_temp.start_x;
+        b_start_y = bg_temp.start_y;
+        b_end_x = bg_temp.end_x;
+        b_end_y = bg_temp.end_y;
 
         if (b_start_x == b_end_x) {
           for (int j = b_start_y; j <= b_end_y; ++j) {
-            doPdList.emplace_back(b_start_x, j);
+            next_do_list.emplace_back(b_start_x, j);
           }
         } else {
           for (int j = b_start_x; j <= b_end_x; ++j) {
-            doPdList.emplace_back(j, b_start_y);
+            next_do_list.emplace_back(j, b_start_y);
           }
         }
       }
     }
 
-    RebuildMap(bingoList[count]);
+    RebuildMap(bingo_list);
+    bingo_list.clear();
 
     for (int i = 0; i < beforeSize; ++i) {
-      doPdList.erase(doPdList.begin());
+      next_do_list.erase(next_do_list.begin());
     }
 
     count++;
   }
-
-  SendData(bingoList);
 }
 
 // 向UI传输数据
-void GameJudge::SendData(std::vector<std::vector<bingoGroup>> bingoList) {
+void GameJudge::SendData(std::vector<std::vector<bingo_group>> draw_acting_list) {
   // TODO  接口协商  emit函数注册
 }
 
-bool GameJudge::FindLengthMore3(int x, int y, bingoGroup &bgG) {
+bool GameJudge::FindLengthMore3(int x, int y, bingo_group &bgG) {
   int sum = 0;
   int target_color = game_map[x][y].GetColorId();
   int start_x, end_x, start_y, end_y;
@@ -114,14 +113,14 @@ int GameJudge::MapDfs(int now_x, int now_y, int target_color, int &t_min_x,
 }
 
 // 交换地图内元素
-void GameJudge::swapDataInMap(int x1, int y1, int x2, int y2) {
+void GameJudge::SwapDataInMap(int x1, int y1, int x2, int y2) {
   Item temp_item(game_map[x1][y1]);
   game_map[x1][y1] = game_map[x2][y2];
   game_map[x2][y2] = temp_item;
 }
 
 // 根据目标List重构地图
-void GameJudge::RebuildMap(std::vector<bingoGroup> bingoList) {
+void GameJudge::RebuildMap(std::vector<bingo_group> bingoList) {
   for (int i = 0; i < bingoList.size(); ++i) {
     int b_start_x, b_start_y, b_end_x, b_end_y;
     b_start_x = bingoList[i].start_x;
--- src/GameJudge.h
+++ src/GameJudge.h
@@ -14,45 +14,45 @@ namespace bjw {
 // 成功消去子的坐标存储结构
 typedef struct node {
   int start_x, end_x, start_y, end_y;
-  bool noInitFlag = false;
+  bool no_init_flag = false;
   node();
   node(int startX, int endX, int startY, int endY) {
     this->start_x = startX;
     this->end_x = endX;
     this->start_y = startY;
     this->end_y = endY;
-    noInitFlag = true;
+    no_init_flag = true;
   }
   void set(int startX, int endX, int startY, int endY) {
     this->start_x = startX;
     this->end_x = endX;
     this->start_y = startY;
     this->end_y = endY;
-    noInitFlag = true;
+    no_init_flag = true;
   }
   bool operator==(node &a) const {
     return a.start_x == start_x && a.end_x == end_x && a.start_y == start_y &&
            a.end_y == end_y;
   }
-} bingoGroup;
+} bingo_group;
 
 typedef struct node1 {
   int x, y;
   node1();
   node1(int x, int y);
 
-} doGroup;
+} do_group;
 
 class GameJudge {
  private:
   std::vector<std::vector<Item>> game_map;  // TODO 实现建图
 
-  bool FindLengthMore3(int, int, bingoGroup &);
+  bool FindLengthMore3(int, int, bingo_group &);
   int MapDfs(int, int, int, int &, int &, int &, int &);
-  void SendData(std::vector<std::vector<bingoGroup>>);
-  void swapDataInMap(int x1, int y1, int x2, int y2);
-  void FillInDataList(bingoGroup);
-  void RebuildMap(std::vector<bingoGroup>);
+  void SendData(std::vector<std::vector<bingo_group>>);
+  void SwapDataInMap(int x1, int y1, int x2, int y2);
+  void FillInDataList(bingo_group);
+  void RebuildMap(std::vector<bingo_group>);
   void RebuildMapBasic(int start, int end, int level, bool heading);
 
  public:
